dba6042a989bb8c390fc5794893c49285088e002
marcusj12 <marcus.a.jennings12@gmail.com>
2022-01-04 19:14:20 -0500

    JavaChallenge #1

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,6 +3,14 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
+        String firstName = "Marcus";
+        String lastName = "Jennings";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
 
     }
+
+
 }
