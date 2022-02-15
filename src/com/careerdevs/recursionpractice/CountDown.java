f2a8932c0f4ad4f9f88164f2c07e92742a257e84
marcusj12 <marcus.a.jennings12@gmail.com>
2022-02-15 15:30:27 -0500

    Java Basics; Count Down algorithm

---
+++ src/com/careerdevs/recursionpractice/CountDown.java
@@ -0,0 +1,33 @@
+package com.careerdevs.recursionpractice;
+
+public class CountDown {
+
+    public static void main(String[] args) {
+        iterative(5);
+    }
+
+    private static void iterative (int countDownFrom) {
+        //option 1
+//        for (int i = 0; i < countDownFrom; i++ ) {
+//            System.out.println(i);
+//        }
+
+//        //option2
+//        for (int i = countDownFrom; i > 0; i--) {
+//            System.out.println(i);
+////        }
+        //option 3
+//        int i = countDownFrom;
+//        while(i >= 1) {
+//            System.out.println(i);
+//            i--;
+//        }
+        //option 4
+        while ((countDownFrom > 0)){
+            System.out.println(countDownFrom);
+            countDownFrom--;
+        }
+    }
+
+
+}
